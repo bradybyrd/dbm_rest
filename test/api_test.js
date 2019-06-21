795291df00c8473f14d742f32aeaa7f82612320b
TeamWork <bradyb@dbmaestro.com>
2019-06-21 11:41:25 -0700

    Parameterize test suites

--- test/api_test.js
+++ test/api_test.js
@@ -1,8 +1,12 @@
 var supertest = require('supertest');
 var should = require('should');
+var testConfig = require('./test_config.json');
+// Set the test scenario Here
+var testScenario = "scenario_1"
+var scenario = testConfig[testScenario]
 
 //Start Listener
-var server = supertest.agent("http://localhost:3001");
+var server = supertest.agent(testConfig.server_url);
 
 // Unit Tests
 
@@ -30,7 +34,7 @@ describe('Basic DBmApi testing', function(){
   });
 
   it('should give a single projects from GET/api/projects/:id', function(done){
-    server.get("/api/projects/1")
+    server.get(`/api/projects/${scenario.project_id}`)
     .expect("Content-type", /json/)
     .end(function(err,res){
       res.status.should.equal(200);
@@ -41,7 +45,7 @@ describe('Basic DBmApi testing', function(){
   });
 
   it('should give environments from GET/api/projects/:id/environments', function(done){
-    server.get("/api/projects/1/environments")
+    server.get(`/api/projects/${scenario.project_id}/environments`)
     .expect("Content-type", /json/)
     .end(function(err,res){
       res.status.should.equal(200);
@@ -51,7 +55,7 @@ describe('Basic DBmApi testing', function(){
   });
 
   it('should give packages from GET/api/projects/:id/packages', function(done){
-    server.get("/api/projects/1/packages")
+    server.get(`/api/projects/${scenario.project_id}/packages`)
     .expect("Content-type", /json/)
     .end(function(err,res){
       res.status.should.equal(200);
@@ -65,8 +69,28 @@ describe('Basic DBmApi testing', function(){
 
 describe('DBm Java CLI actions', function(){
 
-  it('should return a home page', function(done){
-    server.get("/api")
+  it('Performs a deploy from POST/api/projects/:id/packages/:id/deploy{environmentName}', function(done){
+    server.post(`/api/project/${scenario.project_id}/packages/${scenario.package_id}/deploy`)
+    .expect("Content-type", /json/)
+    .end(function(err,res){
+      res.status.should.equal(200);
+      //res.body.error.should.equal(false);
+      done();
+    });
+  });
+
+  it('Performs a package upload from POST/api/projects/:id/packages/upload', function(done){
+    server.post(`/api/project/${scenario.project_id}/packages/upload`)
+    .expect("Content-type", /json/)
+    .end(function(err,res){
+      res.status.should.equal(200);
+      //res.body.error.should.equal(false);
+      done();
+    });
+  });
+
+  it('Performs a validation from POST/api/projects/:id/environments/:id/validate{packageName}', function(done){
+    server.post(`/api/project/${scenario.project_id}/environments/${scenario.environment_id}/validate`)
     .expect("Content-type", /json/)
     .end(function(err,res){
       res.status.should.equal(200);
